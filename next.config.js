4862129ffdfd6918104afcf377cb95687988603d
Kevin <kevin.choi@paust.io>
2019-11-25 16:24:04 +0900

    Add absolute module resolve

---
+++ next.config.js
@@ -0,0 +1,7 @@
+const path = require('path');
+module.exports = {
+  webpack(config) {
+    config.resolve.modules.push(path.resolve('./src'));
+    return config;
+  },
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 import { NextPage } from 'next';
-import Nav from '../components/Nav';
+import Nav from 'components/Nav';
 
 const Home: NextPage = () => (
   <div>
